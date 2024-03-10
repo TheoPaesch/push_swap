24825deb091fdcb8a66328048d312c88aed8d600
Theo Paesch <tpaesch@1-A-14.42heilbronn.de>
2024-03-10 21:19:19 +0100

    implemented more fnctions

--- k_sort.c
+++ k_sort.c
@@ -6,7 +6,7 @@
 /*   By: tpaesch <tpaesch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 13:27:24 by tpaesch           #+#    #+#             */
-/*   Updated: 2024/03/10 20:31:31 by tpaesch          ###   ########.fr       */
+/*   Updated: 2024/03/10 21:15:59 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,12 +53,38 @@ int	move_calc(int *arr_b, int *arr_c)
 
 void	return_to_sender(int *arr_a, int *arr_b, int *arr_c)
 {
+	int	moves;
 
+	while (arr_b[0] != 0)
+	{
+		moves = move_calc(arr_b, arr_c[0]);
+		if (moves > arr_b[0] / 2)
+		{
+			moves = arr_b[0] - moves;
+			while (moves--)
+				rrb(arr_b);
+		}
+		else
+		{
+			while (moves--)
+				rb(arr_b);
+		}
+		pa(arr_a, arr_b);
+		switch_index(arr_c);
+	}
 }
 
 void	switch_index(int *arr_c)
 {
+	int	i;
 
+	i = 1;
+	while (i < arr_c[0])
+	{
+		arr_c[i] = arr_c[i + 1];
+		i++;
+	}
+	arr_c[0]--;
 }
 
 void	k_sort(int *arr_a, int *arr_b, int *arr_c)
